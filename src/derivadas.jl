54c4b436d5b46e3cba511a304627694352cbd7d5
PedroPoffo <pedrohenriquepoffo@gmail.com>
2026-01-28 18:55:05 -0300

    Exemplo com dois elementos e alteração na derivada

--- src/derivadas.jl
+++ src/derivadas.jl
@@ -51,66 +51,76 @@ end
 #####################################################################################
                         # Derivada da frequencia
 #####################################################################################
-function dω(ωn::Vector,U0::Matrix,malha::LFrame.Malha,x::Vector,fdkparam::Function,gdmparam::Function)    
-
-    # numero de frequencia 
-    nω = length(ωn)
+function dω(ωn::Float64,U0::Vector,malha::LFrame.Malha,x::Vector,fdkparam::Function,fdmparam::Function)    
 
     # Iniciando a derivada
-    D = zeros(nω,malha.ne)
+    D = zeros(malha.ne)
 
-    # Alias dos dados da estrutura de malha
+    # Dados da estrutura de malha
     dados_ele = malha.dados_elementos
     dicionario_mat = malha.dicionario_materiais
     dicionario_geo = malha.dicionario_geometrias
     conect = malha.conect
     coordenadas = malha.coord
+    apoios = malha.apoios
     L = malha.L
+    nos = malha.nnos
+   
+    # Iniciando o denominador
+    pm = 0.0
 
-    for i in eachindex(ωn)
+    # Inicializa o vetor com os modos globais
+    ϕg = zeros(nos*6)
+
+    # Descobre quais gdls são livres
+    dofs_l = dofs_livres(nos,apoios)
+
+    # Associa o gdl livre ao vetor de modos
+    ϕg[dofs_l] .= U0
+
+    # Loop pelos elementos
+    for ele in 1:malha.ne
         
-        # Recupera todas as frequencia
-        ω = ωn[i]
+        # Recupera dados do elemento 
+        Iz, Iy, J0, A, α, E, G, geo, ρ = LFrame.Dados_fundamentais(ele, dados_ele, dicionario_mat, dicionario_geo)
 
-        ϕ = U0[:,i]
+        # Rigidez local do elemento 
+        Ke = LFrame.Ke_portico3d(E,Iz,Iy,G,J0,L[ele],A)
 
-        # Loop pelos elementos
-        for ele in 1:malha.ne
+        # Massa local do elemento
+        Me = LFrame.Me_portico3d(A,ρ,L[ele])
+
+        # Derivada da rigidez em relação a variavel
+        dK = Ke*fdkparam(x[ele])
+
+        # Derivada da massa em relação a variavel
+        dM = Me*fdmparam(x[ele])
+         
+        # Graus de liberdades globais do elemento
+        dofs = LFrame.Gls(ele,conect)
         
-            # Recupera dados do elemento 
-            Iz, Iy, J0, A, α, E, G, geo,ρ = LFrame.Dados_fundamentais(ele, dados_ele, dicionario_mat, dicionario_geo)
-
-            # modos no global
-            u0g = ϕ[nω] 
+        # Modos no global
+        ϕ0g = ϕg[dofs]
        
-            # matriz de tranformação de coord(ver se é só rotacao ou translação)
-            T = LFrame.Rotacao3d(ele,conect,coordenadas,α)
-
-            # Para o local
-            u0l = T'*u0g
+        # Matriz de tranformação de coord(ver se é só rotacao ou translação)
+        T = LFrame.Rotacao3d(ele,conect,coordenadas,α)
         
-            # Rigidez local do elemento 
-            Ke = LFrame.Ke_portico3d(E,Iz,Iy,G,J0,L[ele],A)
+        # Para o local
+        ϕ0l = T'*ϕ0g
+    
+        # variavel auxialiar para facilitar
+        aux = dK - (ωn^2)*dM
 
-            # derivada da rigidez em relação a variavel
-            dK = T'*Ke*T*fdkparam(x[ele])
+        # Denominador
+        pm += dot(ϕ0l,Me,ϕ0l)
 
-            # Massa local do elemento
-            Me = LFrame.Me_portico3d(A,ρ,L[ele])
-
-            # derivada da massa em relação a variavel
-            dM = T'*Me*T*gdmparam(x[ele])
-
-            # variavel auxialiar para facilitar
-            aux = dK - (ω^2)*dM
-
-            # denominador
-            pm = dot(u0l,Me,u0l)
-
-            # Deriada da frequencia ωn em relação ao elemento
-            D[i,ele]  =  (1/(2*ω)*pm)*dot(u0l,aux,u0l)
-
-        end
+        # Derivada do elemento
+        D[ele] = dot(ϕ0l,aux,ϕ0l)/(2*ωn)
     end
-    return D
+
+    # Deriadas da frequencia ωn em relação a todos os elemento
+    d = D / pm
+
+    # Retorna a derivada
+    return d
 end
