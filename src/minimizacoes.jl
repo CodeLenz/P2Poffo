160dac128c5dae2081500a5899e601555a89ac0d
PedroPoffo <pedrohenriquepoffo@gmail.com>
2026-06-19 19:47:59 -0300

    up

--- src/minimizacoes.jl
+++ src/minimizacoes.jl
@@ -122,7 +122,7 @@ de um arquivo `.yaml`.
 # Retorno
 Retorna os parâmetros otimizados da malha e as frequências naturais associadas.
 """
-function Main_Otim_Modal(arquivo::AbstractString, fkparam::Function, fdkparam::Function,fmparam::Function, fdmparam::Function,fσparam::Function, fdσparam::Function, posfile=true; verbose=false,n_modos=6, vf = 0.5 ,P=8.0, niter=100,tol_f=1E-4,tol_g=1E-4,s = 2.0)
+function Main_Otim_Modal(arquivo::AbstractString,x=0.5, fkparam=P2Poffo.Kparam, fdkparam=P2Poffo.dKparam,fmparam=P2Poffo.Mparam, fdmparam=P2Poffo.dMparam,fσparam=P2Poffo.σparam, fdσparam=P2Poffo.dσparam, posfile=true; verbose=false,n_modos=6, vf = 0.5 ,P=8.0, niter=100,tol_f=1E-4,tol_g=1E-4,s = 2.0)
 
     # analise modal somente para a malha
     ωn,U0,malha = Modal3D(arquivo,posfile,n_modos=n_modos)
@@ -134,7 +134,7 @@ function Main_Otim_Modal(arquivo::AbstractString, fkparam::Function, fdkparam::F
     ne = malha.ne
 
     # Estimativa inicial das densidades relativas,
-    x0 = 0.9*ones(ne) #vf*ones(ne)
+    x0 = x*ones(ne)
 
     # Calcula o volume de cada elemento sem considerar a 
     # parametrização 
